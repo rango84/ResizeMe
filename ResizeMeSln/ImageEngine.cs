4d79b6bf2872ae5b2e717f3e49134fc6fee53da7
Goran Mancic <gmancic@gmail.com>
2011-09-26 22:21:17 -0400

    first commit

---
+++ ResizeMeSln/ImageEngine.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using System.Windows.Threading;
+
+
+namespace ResizeMe
+{
+    
+    public class ImageEngine
+    {
+        private string m_SourceFolder;
+        private string m_DestFolder;
+        private string m_OutputFile;
+        private double m_ScalePercent;
+        public UpdateProgress progressDelegate;
+        private List<String> FilesToProcess;
+        
+        private ImageEngine() { }
+        public ImageEngine(string sourceFolder, string destFolder,double scale)
+        {
+            m_SourceFolder = sourceFolder;
+            m_DestFolder = destFolder;
+            m_ScalePercent = scale;
+            FilesToProcess = new List<string>();
+        }
+        public void Process() 
+        {
+            int count = 1;
+            foreach (string File in FilesToProcess)
+            {
+                m_OutputFile = m_DestFolder + "\\" + Path.GetFileName(File);
+                progressDelegate.Invoke(count, File);
+                Bitmap img = new Bitmap(File);
+                Size size = new Size(0, 0);
+                Bitmap newImage = resizeImage(img, size, (float)m_ScalePercent);
+                SaveImage(newImage, 100L);
+                img.Dispose();
+                newImage.Dispose();
+                count++;
+                
+            }
+            
+        }
+
+        private void SaveImage(Bitmap img, long quality) 
+        {
+            
+            EncoderParameter qty = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+            ImageCodecInfo jpegCodec = getEncoderInfo("image/jpeg");
+
+            if (jpegCodec == null)
+                return;
+
+            EncoderParameters encoderParam = new EncoderParameters(1);
+            encoderParam.Param[0] = qty;
+
+            img.Save(m_OutputFile, jpegCodec, encoderParam);
+            img.Dispose();
+        }
+
+        private ImageCodecInfo getEncoderInfo(string mimeType) 
+        {
+            ImageCodecInfo [] codecs = ImageCodecInfo.GetImageEncoders();
+
+            for (int i = 0; i < codecs.Length; ++i)
+            {
+                if (codecs[i].MimeType == mimeType)
+                    return codecs[i];
+            }
+            return null;
+        }
+
+        private Bitmap resizeImage(Image originalImage, Size size, float percent) 
+        {
+            
+            int sourceWidth = originalImage.Width;
+            int sourceHeigh = originalImage.Height;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            // need to implement GUI to use size as well 
+            if (!(size.Height <= 0) || ! (size.Width <= 0))
+            {
+                nPercentW = ((float)size.Width / (float)sourceWidth);
+                nPercentH = ((float)size.Height / (float)sourceHeigh);
+            }
+
+            if (percent >= 0.0)
+                nPercent = (float)(percent / 100.0);
+            else
+            {
+                if (nPercentH < nPercentW)
+                    nPercent = nPercentH;
+                else
+                    nPercent = nPercentW;
+            }
+            
+
+            int destW = (int)(sourceWidth * nPercent);
+            int destH = (int)(sourceHeigh * nPercent);
+
+            Bitmap newImg = new Bitmap(destW, destH);
+            Graphics g = Graphics.FromImage((Image)newImg);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+            g.DrawImage(originalImage, 0, 0, destW, destH);
+            g.Dispose();
+
+            return newImg;
+        }
+
+        public void GetFilesInDirectory()
+        {
+            string[] files = Directory.GetFiles(m_SourceFolder);
+            for (int i = 0; i != files.Length; ++i)
+            {
+                string ext = System.IO.Path.GetExtension(files[i]);
+                if (ext.ToLower() == ".jpg")
+                    FilesToProcess.Add(System.IO.Path.GetFullPath(files[i]));
+            }
+        }
+    }
+
+    public static class Helper 
+    {
+        public static int GetValidFilesInDirectory(string folder)
+        {
+            string[] files = Directory.GetFiles(folder);
+            int count =0;
+            for (int i = 0; i != files.Length; ++i)
+            {
+                string ext = System.IO.Path.GetExtension(files[i]);
+                if (ext.ToLower() == ".jpg")
+                    count++;   
+            }
+            return count;
+        }
+    }
+}
---
+++ ResizeMeSln/MainWindow.xaml.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.ComponentModel;
+using System.Runtime.Remoting.Messaging;
+namespace ResizeMe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public delegate void UpdateProgress(int progress, string file);
+    public partial class MainWindow : Window
+    {
+
+        public delegate void WorkerDelegate(double scale);
+        
+        string m_SourceFolder;
+        string m_DestFolder;
+        List<string> FilesToProcess;
+        private WorkerDelegate asynchDelegate;
+        private int m_MaxFiles;
+        public MainWindow()
+        {
+            InitializeComponent();
+            FilesToProcess = new List<string>();   
+        }
+
+        private void OpenFolder(FolderType FolderType)
+        {
+            try
+            {
+                FolderBrowserDialog Open = new FolderBrowserDialog();
+                if (Open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    switch (FolderType)
+                    {
+                        case MainWindow.FolderType.Destination:
+                            m_DestFolder = Open.SelectedPath;
+                            textBoxDest.Text = m_DestFolder;
+                            break;
+                        case MainWindow.FolderType.Source:
+                            m_SourceFolder = Open.SelectedPath;
+                            textBoxSource.Text = m_SourceFolder;
+                            break;
+                        default:
+                            throw new Exception("Unknown Folder Type");
+                    }
+                }
+            }
+            catch (Exception ex) 
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+            }
+        }
+        
+        private void Browse_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.Source == DestBtnBrowse)
+                OpenFolder(FolderType.Destination);
+            else if (e.Source == SrcBtnBrowse)
+                OpenFolder(FolderType.Source);
+            else
+                System.Windows.MessageBox.Show("Invalid Operation");
+        }
+
+        public enum FolderType 
+        {
+            Destination,
+            Source
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(m_DestFolder) || String.IsNullOrEmpty(m_SourceFolder))
+            {
+                System.Windows.MessageBox.Show("You need to select destination and source folders");
+                return;
+            }
+            try
+            {
+                double scale = ParseScale();
+                if (scale == 0)
+                    return;
+                if ((m_MaxFiles= Helper.GetValidFilesInDirectory(m_SourceFolder)) == 0) 
+                {
+                    System.Windows.MessageBox.Show("No Jpeg files found");
+                    return;
+                }
+                fileProgressBar.Maximum = m_MaxFiles;
+                asynchDelegate = new WorkerDelegate(Resize);
+                IAsyncResult res = asynchDelegate.BeginInvoke(scale,callback,null);
+                lblStatus.Content = "Resizing....";
+                btnGo.IsEnabled = false;
+            }
+            catch (Exception ex) 
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void callback(IAsyncResult res)
+        {
+            System.Windows.MessageBox.Show("Resizing is completed");
+            Dispatcher.Invoke((Action)delegate() 
+                { 
+                    lblStatus.Content = "Finished";
+                    btnGo.IsEnabled = true;
+                    fileProgressBar.Value = 0;
+                    lblCurrentFile.Content = String.Empty;
+                }, null);
+        }
+        public void Resize(double scale) 
+        {
+               
+            ImageEngine imgEng = new ImageEngine(m_SourceFolder, m_DestFolder,scale);
+            imgEng.progressDelegate += new UpdateProgress(UpdateUI);
+            imgEng.GetFilesInDirectory();
+            imgEng.Process();
+        }
+
+        public void UpdateUI(int progress, string file) 
+        {
+            Dispatcher.Invoke((Action)delegate() 
+            {
+                UpdateProgressBar(progress);
+                UpdateFileLabel(file);
+            }, null);   
+        }
+
+        public void UpdateProgressBar(int val) 
+        {
+            fileProgressBar.Value = val;
+        }
+        public void UpdateFileLabel(string File) 
+        {
+            lblCurrentFile.Content = File;
+        }
+
+        private double ParseScale() 
+        {
+            double val = 0;
+            val = Convert.ToDouble(textBoxScale.Text);
+            return val;
+        }
+
+        private int GetNumberOfFiles()
+        {
+            return Directory.GetFiles(m_SourceFolder).Length;
+        }
+
+        private void _Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            new AboutResizeMe().ShowDialog();
+
+        }
+    }
+}
